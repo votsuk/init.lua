99dba364341e0555051f9643e036d547db143c90
Steven <kustovsteven@gmail.com>
2025-09-23 21:39:27 -0400

    Update lazy-lock.json dependencies, add HTML tag remap, and clean up LSP and Treesitter configurations

--- lua/votsuk/lazy/treesitter.lua
+++ lua/votsuk/lazy/treesitter.lua
@@ -4,7 +4,6 @@ return {
 		build = ":TSUpdate",
 		config = function()
 			require("nvim-treesitter.configs").setup({
-				-- A list of parser names, or "all"
 				ensure_installed = {
 					"astro",
 					"bash",
@@ -38,10 +37,10 @@ return {
 					-- `false` will disable the whole extension
 					enable = true,
 					disable = function(lang, buf)
-						if lang == "html" or lang == "go" then
-							print("disabled")
-							return true
-						end
+                        if lang == "html" then
+                            print("disabled")
+                            return true
+                        end
 
 						local max_filesize = 100 * 1024 -- 100 KB
 						local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
@@ -62,20 +61,8 @@ return {
 					additional_vim_regex_highlighting = { "markdown" },
 				},
 			})
-
-			local treesitter_parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-			treesitter_parser_config.templ = {
-				install_info = {
-					url = "https://github.com/vrischmann/tree-sitter-templ.git",
-					files = { "src/parser.c", "src/scanner.c" },
-					branch = "master",
-				},
-			}
-
-			vim.treesitter.language.register("templ", "templ")
 		end,
 	},
-
 	{
 		"nvim-treesitter/nvim-treesitter-context",
 		after = "nvim-treesitter",
